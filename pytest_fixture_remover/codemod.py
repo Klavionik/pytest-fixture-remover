c426158c10f84d6c1cc732e0ade30dacedd9a0b5
Roman Vlasenko <klavionik@gmail.com>
2024-02-11 01:41:03 +0300

    Refactor

--- pytest_fixture_remover/codemod.py
+++ pytest_fixture_remover/codemod.py
@@ -12,32 +12,51 @@ class RemovePytestFixtureCommand(VisitorBasedCodemodCommand):
         Removes usages of a pytest fixture via the `usefixtures` decorator and its parametrization.
     """
 
+    def __init__(self, context: CodemodContext, name: str) -> None:
+        super().__init__(context)
+        self.name = name
+
     @staticmethod
     def add_args(arg_parser: argparse.ArgumentParser) -> None:
         arg_parser.add_argument(
             "--name", dest="name", help="Fixture to remove.", type=str, required=True
         )
 
-    def __init__(self, context: CodemodContext, name: str) -> None:
-        super().__init__(context)
-        self.name = name
+    def get_usefixtures_matcher(self) -> m.Call:
+        """
+        Returns a matcher for a pytest `usefixtures` call
+        that has a fixture in question as one of its arguments.
+        :return: Call matcher.
+        """
+        return m.Call(
+            func=m.Attribute(attr=m.Name("usefixtures")),
+            args=[
+                m.ZeroOrMore(m.DoNotCare()),
+                m.AtLeastN(n=1, matcher=m.Arg(m.SimpleString(f'"{self.name}"'))),
+                m.ZeroOrMore(m.DoNotCare()),
+            ],
+        )
+
+    def get_parametrize_matcher(self) -> m.Call:
+        """
+        Returns a matcher for a pytest `parametrize` call
+        that defines parameters for a fixture in question.
+        :return: Call matcher.
+        """
+        return m.Call(
+            func=m.Attribute(attr=m.Name("parametrize")),
+            args=[
+                m.Arg(m.SimpleString(f'"{self.name}"')),
+                m.ZeroOrMore(m.DoNotCare()),
+            ],
+        )
 
     def leave_Decorator(
         self, original_node: "Decorator", updated_node: "Decorator"
     ) -> Union["Decorator", FlattenSentinel["Decorator"], RemovalSentinel]:
-        if m.matches(
-            updated_node.decorator,
-            m.Call(
-                func=m.Attribute(attr=m.Name("usefixtures")),
-                args=[
-                    m.ZeroOrMore(m.DoNotCare()),
-                    m.AtLeastN(n=1, matcher=m.Arg(m.SimpleString(value=f'"{self.name}"'))),
-                    m.ZeroOrMore(m.DoNotCare()),
-                ],
-            ),
-        ):
+        if m.matches(updated_node.decorator, self.get_usefixtures_matcher()):
             for child in updated_node.decorator.children:
-                if m.matches(child, m.Arg(m.SimpleString(value=f'"{self.name}"'))):
+                if m.matches(child, m.Arg(m.SimpleString(f'"{self.name}"'))):
                     node = updated_node.deep_remove(child)
                     if m.matches(node.decorator, m.Call(args=[])):
                         return libcst.RemoveFromParent()
@@ -49,15 +68,6 @@ class RemovePytestFixtureCommand(VisitorBasedCodemodCommand):
                                 return node
                     return node
 
-        if m.matches(
-            updated_node.decorator,
-            m.Call(
-                func=m.Attribute(attr=m.Name("parametrize")),
-                args=[
-                    m.Arg(m.SimpleString(f'"{self.name}"')),
-                    m.ZeroOrMore(m.DoNotCare()),
-                ],
-            ),
-        ):
+        if m.matches(updated_node.decorator, self.get_parametrize_matcher()):
             return libcst.RemoveFromParent()
         return updated_node
